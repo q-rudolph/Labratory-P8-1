20231b76a1a4942885c7522096b35e4084806773
Quentin Rudolph <qjrudolph@icloud.com>
2025-10-23 12:27:21 -0400

    Created countVowel function.

--- Labratory P8-1/Labratory P8-1.cpp
+++ Labratory P8-1/Labratory P8-1.cpp
@@ -3,15 +3,28 @@
 
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 int main() {
 	string str;
-	cout << "Enter a string: ";
-	getline(cin, str);
-	int count = countCharacter(str);
-	cout << "The number of characters in the string is: " << count << endl;
-	return 0;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, str);
+		if (str=="Q" || str=="q") {
+			break;
+		}
+		int count = countVowel(str);
+		cout << "Vowel count: " << count << endl;
+	}
 }
-int countCharacter(string str) {
-	return str.length();
+int countVowel(string str) {
+	int count = 0;
+	string vowels = "aeiouAEIOU";
+	for (int i = 0; i < str.length(); i++) {
+		for (int j = 0; j < vowels.length(); j++) {
+			if (str[i] == vowels[j]) {
+				count++;
+			}
+		}
+	}
+	return count;
 }
